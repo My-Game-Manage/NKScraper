d794a71b0adce47ec7aa01c894f406f4653d7408
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-27 15:40:17 +0900

    Update netkeiba_client.py

--- src/netkeiba_client.py
+++ src/netkeiba_client.py
@@ -8,3 +8,15 @@ from selenium.common.exceptions import WebDriverException
 class NetKeibaClient:
     def __init__(self, headless=True):
         self.driver = self._setup_driver(headless)
+
+     def _setup_driver(self, headless):
+        options = Options()
+        if headless:
+            options.add_argument('--headless=new')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.page_load_strategy = 'eager'
+        
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        driver.set_page_load_timeout(30)
+        return driver
