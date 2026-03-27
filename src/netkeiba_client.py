bad7898049f2f9ed11b1561fccdc15fa7302abd4
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-27 15:30:11 +0900

    Update netkeiba_client.py

--- src/netkeiba_client.py
+++ src/netkeiba_client.py
@@ -1,1 +1,10 @@
 # netkeibaアクセス用
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException
+
+class NetKeibaClient:
+    def __init__(self, headless=True):
+        self.driver = self._setup_driver(headless)
