ac223d54fffeb252d435858fe13d1019751fb6ff
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-27 21:17:58 +0900

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -3,21 +3,10 @@
 import re
 from bs4 import BeautifulSoup
 
-from src.constants.master_data import JYO_NAME_MAP
-
 class DataParser:
     def __init__(self):
         self._any = None
         
-    def get_jyo_name(self, kaisai_id: str) -> str:
-        """10桁または12桁のIDから会場名を特定"""
-        if not kaisai_id or len(kaisai_id) < 6:
-            return "不明"
-            
-        code = kaisai_id[4:6]
-        # 定数から取得。なければ "不明" を返す
-        return JYO_NAME_MAP.get(code, "不明")
-
     def extract_race_ids(self, html_content: str) -> list:
         """
         開催トップページのHTMLからその日のレースID一覧を抽出する
