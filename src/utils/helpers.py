f1ddeadc03b643ae8464c7b0e4a879f4dde31471
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-28 11:44:36 +0900

    Update helpers.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -55,11 +55,11 @@ def get_race_url(race_id: str, page_type: NetkeibaPageType=NetkeibaPageType.SHUT
     
     return f"https://{domain}.netkeiba.com/{path}?race_id={race_id}"
 
-def get_horse_url() -> str:
+def get_horse_url(horse_id: str) -> str:
     """
     馬IDからフルURLを生成する
     """
-    return f"https://db.netkeiba.com/horse/{h_id}"
+    return f"https://db.netkeiba.com/horse/{horse_id}"
 
 def get_top_page_url(target_date: str, is_nar: bool = True) -> str:
     """
