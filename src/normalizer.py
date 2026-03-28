30066869bf51aba5c572f4fcaf10c2e9cb6a47db
N.T.WORKS <nagisc007@yahoo.co.jp>
2026-03-28 14:42:55 +0900

    Update normalizer.py

--- src/normalizer.py
+++ src/normalizer.py
@@ -16,6 +16,21 @@ class DataNormalizer:
             df[RaceCol.TIME] = df[RaceCol.TIME].apply(time_to_seconds)
             
         return df
+
+    @staticmethod
+    def ensure_dataframe(data) -> pd.DataFrame:
+    """
+    入力がリストならDataFrameに変換し、DataFrameならそのまま返す。
+    それ以外（Noneなど）の場合は空のDataFrameを返す。
+    """
+    if isinstance(data, pd.DataFrame):
+        return data
+    
+    if isinstance(data, list):
+        return pd.DataFrame(data)
+    
+    # データが空、または想定外の型の場合
+    return pd.DataFrame()
         
     @staticmethod
     def normalize_columns(df: pd.DataFrame) -> pd.DataFrame:
